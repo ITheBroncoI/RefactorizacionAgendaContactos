cf3b3bb97bfa0143b270183f5690c6fe80f8f5ad
iTHeBroncoi <angelvolkenturbo15@gmai.com>
2025-03-24 22:18:44 -0600

    Refactorizacion de la clase Contact, se agregan Getters y Setter, y se encapsulan las variables

--- src/main/java/org/example/Contact.java
+++ src/main/java/org/example/Contact.java
@@ -1,11 +1,29 @@
 package org.example;
 
 class Contact {
-    public String name;
-    public String phone;
+    private String name; // Se ponen las variables en privado, cumpliendo el principio de encapsulamiento
+    private String phone;
 
-    Contact(String name, String phone) {
+    // Constructor
+    public Contact(String name, String phone) {
         this.name = name;
+        this.phone = phone;
+    }
+
+    // Se agregan setters y getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
